aecc9061c9784de80885dc451ed414185bbdf38d
Caroline Muscara <cjm72@bu.edu>
2025-04-28 12:59:47 -0400

    minor changes to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import CocktailCard from './components/CocktailCard';
+import CocktailDetail from './components/CocktailDetail';
+import Header from './components/Header';
+import SearchBar from './components/SearchBar';
+
 
 function App() {
     return (
-        <>
-        </>
+        <Router>
+            <Routes>
+
+            </Routes>
+        </Router>
     );
 }
 
