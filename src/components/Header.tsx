4340677100d9f4a3000aa7efbe01606a628432ea
Mauricio Szajman <mausza@bu.edu>
2025-04-28 18:59:16 -0400

    removed styling for removed nav components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -27,45 +27,6 @@ const Logo = styled.h1`
   }
 `;
 
-const Nav = styled.nav`
-  @media screen and (max-width: 750px) {
-    width: 100%;
-  }
-`;
-
-const NavList = styled.ul`
-  display: flex;
-  gap: 2vw;
-  list-style: none;
-  padding: 0;
-  margin: 0;
-  @media screen and (max-width: 750px) {
-    flex-direction: row;
-    background-color: white;
-    width: 100%;
-    justify-content: center;
-  }
-`;
-
-const NavItem = styled.li`
-  margin: 4vh auto;
-  @media screen and (max-width: 750px) {
-    font-size: calc(1.5px + 1.5vw);
-    margin: 1% 1%;
-  }
-`;
-
-const NavLink = styled(Link)`
-  color: #3e2723;
-  text-decoration: none;
-  font-weight: 500;
-  font-size: 1.1rem;
-  transition: color 0.2s;
-
-  &:hover {
-    color: #d35400;
-  }
-`;
 
 export default function Header() {
   return (
